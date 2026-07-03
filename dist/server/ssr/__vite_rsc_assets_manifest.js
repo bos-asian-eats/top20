4933d860b33690c65ffa1526b9bef8e5bfc0dce1
bos-asian-eats <sanmita.shah@gmail.com>
2026-07-03 13:28:26 -0400

    Add files via upload

---
+++ dist/server/ssr/__vite_rsc_assets_manifest.js
@@ -0,0 +1,38 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-8eaY3GsO.js\")",
+  "clientReferenceDeps": {
+    "7fcd9d86d73f": {
+      "js": [
+        "/assets/index-8eaY3GsO.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    },
+    "6f7386b31f5d": {
+      "js": [
+        "/assets/layout-segment-context-C_CIB2fq.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/index-8eaY3GsO.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    },
+    "7f97fa5b0c6c": {
+      "js": [
+        "/assets/index-8eaY3GsO.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-Dzbg7ezJ.css"
+      ]
+    }
+  }
+}
